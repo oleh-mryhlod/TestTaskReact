d8ad2c98d908cb664f38efceef8ba77c3092bbaa
oleh-mryhlod <oleg.mryglod@gmail.com>
2017-11-05 21:40:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.set("port", process.env.PORT || 3001);
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use('/', express.static("client/build"));
 }
 
 
