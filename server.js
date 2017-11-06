1e356f81d2b9f780997da240088cf36ebf5e8335
oleh-mryhlod <oleg.mryglod@gmail.com>
2017-11-06 19:06:59 +0200

    Fixed search by url

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const Twitter = require('twitter')
 const {CONSUMER_KEY, CONSUMER_SECRET, BEARER_TOKEN} = require('./config')
+const path = require('path')
 
 const app = express();
 
@@ -12,13 +13,6 @@ const client = new Twitter({
 
 
 app.set("port", process.env.PORT || 3001);
-
-
-// Express only serves static assets in production
-if (process.env.NODE_ENV === "production") {
-  app.use('/', express.static("client/build"));
-}
-
 
 app.get("/api/search", (req, res) => {
   const param = req.query.q;
@@ -33,6 +27,16 @@ app.get("/api/search", (req, res) => {
     res.json(tweets);
   });
 });
+
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === "production") {
+  const staticFiles = express.static("client/build")
+  app.use(staticFiles)
+  app.get('*', function(req, res) {
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+  });
+}
 
 app.listen(app.get("port"), () => {
   console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
